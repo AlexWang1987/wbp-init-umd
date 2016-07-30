f1a842fed6da42ff32d23690eb45ce31233e2736
AlexWang <1669499355@qq.com>
2016-07-30 15:28:12 +0800

    local_dev_commit

--- assets/webpack.config.umd.js
+++ assets/webpack.config.umd.js
@@ -2,6 +2,8 @@ module.exports = function (umdConf) {
   // enableHotReload
   umdConf.enableHotReplacement('main');
 
+  // addExternal
+
   // all dependencies are added as venders default
   // umdConf.addVendor(Object.keys(umdConf.pkg.dependencies).map(function (depen_name) {
   //   return require.resolve(depen_name);
