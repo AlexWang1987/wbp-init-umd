e16b55e29f3b52d53d1b180d05d3e46f51016a74
AlexWang <1669499355@qq.com>
2016-09-11 17:46:39 +0800

    local_dev_commit

--- assets/webpack.config.umd.js
+++ assets/webpack.config.umd.js
@@ -1,10 +1,10 @@
 module.exports = function (umdConf) {
   umdConf.devServer.host = '0.0.0.0';
   umdConf.webpackFeatures.enableEntryHTML();
+  umdConf.output.publicPath = '';
 
   if (umdConf.devMode) {
     umdConf.webpackFeatures.enableEntryHot();
-    umdConf.output.publicPath = '';
   } else {
     umdConf.webpackFeatures.enableUglifyJs({
       comments: false
